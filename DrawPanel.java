3ffc9859178623b8d02427a492f7fd076671b21f
Taha Ali <tahaali2790@gmail.com>
2017-10-29 21:35:07 -0700

    Add files via upload

---
+++ DrawPanel.java
@@ -0,0 +1,31 @@
+package first_Application;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+
+public class DrawPanel extends JPanel 
+{
+
+//	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		public void paintComponent( Graphics g )
+		{
+			 
+//			g.drawLine( 10, 20, 100, 400 );
+			
+//			public void paint(Graphics g) {
+			    g.setColor(Color.red);
+			    
+			    g.drawRect(10,10,100,100);
+			  }
+				}
+		
+		
+		
+		
+	
+
+
